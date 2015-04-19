c6e528c6e33191c6cea5dd649925547650cebc76
Bob <bobnmfrog@gmail.com>
2015-04-19 20:15:13 +0900

    人画像追加

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -34,7 +34,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto director = Director::getInstance();
     auto glview = director->getOpenGLView();
     if(!glview) {
-        glview = GLViewImpl::createWithRect("GoldRush", Rect(0,0,320 * 3, 240 * 3));
+        glview = GLViewImpl::createWithRect("Forty-niners", Rect(0,0,320 * 3, 240 * 3));
         director->setOpenGLView(glview);
     }
 
